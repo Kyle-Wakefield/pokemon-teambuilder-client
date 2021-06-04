87c766092e0a18132e1ae691e35a5cfc57718863
sillykyle <sillykylew@yahoo.com>
2021-06-04 15:35:50 -0400

    Add heroku url to apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://afternoon-tundra-85143.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
